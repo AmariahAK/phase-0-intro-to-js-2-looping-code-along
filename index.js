847efdaf1fda8bc967319e7cfb853fd5b440488c
Amariah <amariah.abish@gmail.com>
2024-03-14 12:00:54 +0300

    Js looping test

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Code your solutions in this file
-function countDown(num) {
+// Function to generate thank you messages for each name provided
+function writeCards(names, event) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a positive integer to zero
+  function countDown(num) {
     if (num <= 0) {
       console.log("Please provide a positive integer.");
       return;
@@ -11,6 +23,4 @@ function countDown(num) {
     }
   }
   
-  // Example usage:
-  countDown(10);
   
