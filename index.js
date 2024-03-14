a41b7bc4e2b6eab9d5fb507d37cdc063defebc4f
Amariah <amariah.abish@gmail.com>
2024-03-14 11:58:24 +0300

    Js Looping test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num) {
+    if (num <= 0) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  // Example usage:
+  countDown(10);
+  
